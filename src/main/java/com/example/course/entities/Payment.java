1e8b80a98b6623d1e950f2d65392daa43c854dfe
LucasLopesFrazao <lucaslopesfrazao2003@gmail.com>
2022-05-11 11:28:35 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/Payment.java
+++ src/main/java/com/example/course/entities/Payment.java
@@ -12,8 +12,10 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
-@Table(name = "")
+@Table(name = "tb_payment")
 public class Payment implements Serializable{
 	private static final long serialVersionUID = 1L;
 
@@ -53,6 +55,7 @@ public class Payment implements Serializable{
 		this.moment = moment;
 	}
 
+	@JsonIgnore
 	public Order getOrder() {
 		return order;
 	}
